a5492aed726ef6b3ac48e6a5bc1f03bfb6abcc2d
Felipe Cook <felipepcook@gmail.com>
2018-12-24 17:16:45 -0700

    New project files, with file implementing "Hello World" program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * @author Felipe Cook
+ * Simple class that displays the Hello World message
+ */
+public class HelloWorld {
+
+  /**
+   * Prints "Hello World!" to standard output.
+   * 
+   * @param args Command-line parameters (ignored).
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
